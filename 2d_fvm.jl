d85a4c2c0fa4eebdfc18d80add91487a684cb6c9
Aniket <ani.kulkar@gmail.com>
2023-09-06 15:30:39 +0530

    bc implemented

--- 2d_fvm.jl
+++ 2d_fvm.jl
@@ -8,84 +8,56 @@ include("./functions_fvm.jl")
 f = open("settings.json", "r")
 settings = JSON.parse(f)
 
+temperature_base_units = settings["units"]["temperature"]
 num_sources = settings["model"]["num_sources"]
 xy_mesh, u0, N = build_domain(settings, num_sources)
+mesh_size = step(xy_mesh)
+
+u0 .= convert_units("temperature", settings["IC"], temperature_base_units, "K")
 
 source = zeros(N,N)
-source[10:20, 10:20] .= 10.0
+source[10:20, 10:20] .= 1
 
 println(Dates.format(now(), "HH:MM:SS"))
 
 function conduction_2d_loop!(du, u, p, t)
-    source, alpha, dx = p
-    alpha = alpha / dx^2
-    
-    if settings["BC"]["X-"]["type"] == "constant_T"
-        u[1,:] .= 2*settings["BC"]["X-"]["value"] .- u[2,:]
-    elseif settings["BC"]["X-"]["type"] == "insulated"
-        u[1,:] .= u[2,:]
-    end
-    if settings["BC"]["X+"]["type"] == "constant_T"
-        u[end,:] .= 2*settings["BC"]["X+"]["value"] .- u[end-1,:]
-    elseif settings["BC"]["X+"]["type"] == "insulated"
-        u[end,:] .= u[end-1,:]
-    end
-
-    if settings["BC"]["Y-"]["type"] == "constant_T"
-        u[:,1] .= 2*settings["BC"]["Y-"]["value"] .- u[:,2]
-    elseif settings["BC"]["Y-"]["type"] == "insulated"
-        u[:,1] .= u[:,2]
-    end
-    if settings["BC"]["Y+"]["type"] == "constant_T"
-        u[:,end] .= 2*settings["BC"]["Y+"]["value"] .- u[:,end-1]
-    elseif settings["BC"]["Y+"]["type"] == "insulated"
-        u[:,end] .= u[:,end-1]
-    end
+    source, alpha, k, rho, cp, mesh_size = p
+    alpha = alpha / mesh_size^2
 
     @inbounds for i in range(2, N-1)
         @inbounds for j in range(2, N-1)
-            # ip1, im1, jp1, jm1 = limit(i + 1, N), limit(i - 1, N), limit(j + 1, N), limit(j - 1, N)
             ip1, im1, jp1, jm1 = i + 1, i - 1, j + 1, j - 1
-            # if im1 == 0
-            #     du[i, j] = alpha * (u[im1, j] + u[ip1, j] + u[i, jp1] + u[i, jm1] - 4u[i, j]) + source[i, j]
-            # elseif ip1 == x_end
-            #     du[i, j] = alpha * (u[im1, j] + u[ip1, j] + u[i, jp1] + u[i, jm1] - 4u[i, j]) + source[i, j]
-            # elseif 
             du[i, j] = alpha * (u[im1, j] + u[ip1, j] + u[i, jp1] + u[i, jm1] - 4u[i, j]) + source[i, j]
         end
     end
-    # du[:,1] = du[:, end] .= 1
+
+    apply_bc(u, settings, mesh_size)
 
 end
-p = (source, 1, step(xy_mesh))
 
-# function init_conduction_2d!(xyd)
-#     N = length(xyd)
-#     u = zeros(N, N)
-#     return u
-# end
-# u0 = init_conduction_2d!(xy_mesh)
-# prob_conduction_2d = ODEProblem(conduction_2d_loop!, u0, (0.0, 10), p)
+k = settings["model"]["bodies"]["die"]["material"]["k"]
+rho = settings["model"]["bodies"]["die"]["material"]["rho"]
+cp = settings["model"]["bodies"]["die"]["material"]["cp"]
+alpha = k/(rho*cp)
 
+p = (source, alpha, k, rho, cp, mesh_size)
 
-du0 = copy(u0)
+du0 = zeros(size(u0))
 jac_sparsity = Symbolics.jacobian_sparsity((du, u) -> conduction_2d_loop!(du, u, p, 0.0),
                                            du0, u0)
 
 f = ODEFunction(conduction_2d_loop!; jac_prototype = float.(jac_sparsity))
-prob_conduction_2d_sparse = ODEProblem(f, u0, (0.0, 10.0), p, saveat=1.0)
-# using BenchmarkTools # for @btime
-# solve(prob_conduction_2d, TRBDF2(), saveat=1.0);
-# sol = solve(prob_conduction_2d_sparse, TRBDF2(), save_everystep = false, saveat=1.0)
+prob_conduction_2d_sparse = ODEProblem(f, u0, (0.0, 1000.0), p)
+
 sol = solve(prob_conduction_2d_sparse, 
             KenCarp47(linsolve = KLUFactorization()),
             save_everystep = false, 
-            saveat=1.0);
+            saveat=100.0);
 
 println(Dates.format(now(), "HH:MM:SS"))
+result_ = convert_units("temperature", sol[end][2:end-1,2:end-1], "K", "C")'
 
-
-plot(contour(x=xy_mesh,y=xy_mesh,z=sol[end][2:end-1,2:end-1]', colorscale="Jet",colorbar=attr(
+plot(contour(x=xy_mesh,y=xy_mesh,z=result_, colorscale="Jet",colorbar=attr(
     title="Temperature", # title here
     titleside="right",
     titlefont=attr(

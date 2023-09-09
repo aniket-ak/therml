7341bf53d4882a362bfd7bcab4d8213a3d0bcf71
Aniket <ani.kulkar@gmail.com>
2023-09-09 05:40:26 +0530

    add installation script

---
+++ installation_.jl
@@ -0,0 +1,8 @@
+using Pkg
+
+Pkg.add("DifferentialEquations")
+Pkg.add("CUDA")
+Pkg.add("JSON")
+Pkg.add("PlotlyJS")
+Pkg.add("LoopVectorization")
+Pkg.add("Symbolics")
---
+++ thermal_installer.sh
@@ -0,0 +1,5 @@
+wget https://julialang-s3.julialang.org/bin/linux/x64/1.9/julia-1.9.3-linux-x86_64.tar.gz
+tar zxvf julia-1.9.3-linux-x86_64.tar.gz
+
+export PATH="$PATH:/root/julia-1.9.3/bin"
+julia installation_.jl

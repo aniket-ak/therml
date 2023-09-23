eeac6838fccd0f9a7310fb43d2376cc7c705dcbe
Aniket Abhay Kulkarni <ani.kulkar@gmail.com>
2023-09-23 06:33:21 +0530

    Update app.py

--- UI/app.py
+++ UI/app.py
@@ -1,10 +1,12 @@
 import dash
 import dash_bootstrap_components as dbc
 from dash import dcc
-from dash import html
+from dash import Input, Output, State, html
 from dash.dependencies import Input, Output
 
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
+# app.stylesheets.serve_locally=True
+app.scripts.serve_locally = True
 
 header = html.H1(
     "MarlinSim TherML"
@@ -14,53 +16,75 @@ left_accordion = dbc.Accordion([
     dbc.AccordionItem([
         html.Label("Mold"),
         dbc.Row([
-            dbc.Col([dbc.Input(placeholder="X", type="number", required=True, size="sm")], width=4),
-            dbc.Col([dbc.Input(placeholder="Y", type="number", required=True, size="sm")], width=4),
-            dbc.Col([dbc.Input(placeholder="Z", type="number", required=True, size="sm")], width=4),
+            dbc.Col([dbc.Input(placeholder="X", type="number",
+                    required=True, size="sm")], width=4),
+            dbc.Col([dbc.Input(placeholder="Y", type="number",
+                    required=True, size="sm")], width=4),
+            dbc.Col([dbc.Input(placeholder="Z", type="number",
+                    required=True, size="sm")], width=4),
         ]),
 
         html.Label("Die"),
         dbc.Row([
-            dbc.Col([dbc.Input(placeholder="X", type="number", required=True, size="sm")], width=4),
-            dbc.Col([dbc.Input(placeholder="Y", type="number", required=True, size="sm")], width=4),
-            dbc.Col([dbc.Input(placeholder="Z", type="number", required=True, size="sm")], width=4),
+            dbc.Col([dbc.Input(placeholder="X", type="number",
+                    required=True, size="sm")], width=4),
+            dbc.Col([dbc.Input(placeholder="Y", type="number",
+                    required=True, size="sm")], width=4),
+            dbc.Col([dbc.Input(placeholder="Z", type="number",
+                    required=True, size="sm")], width=4),
         ]),
 
         html.Label("Underfill"),
         dbc.Row([
-            dbc.Col([dbc.Input(placeholder="X", type="number", required=True, size="sm")], width=4),
-            dbc.Col([dbc.Input(placeholder="Y", type="number", required=True, size="sm")], width=4),
-            dbc.Col([dbc.Input(placeholder="Z", type="number", required=True, size="sm")], width=4),
+            dbc.Col([dbc.Input(placeholder="X", type="number",
+                    required=True, size="sm")], width=4),
+            dbc.Col([dbc.Input(placeholder="Y", type="number",
+                    required=True, size="sm")], width=4),
+            dbc.Col([dbc.Input(placeholder="Z", type="number",
+                    required=True, size="sm")], width=4),
         ]),
 
         html.Label("Bumps"),
         dbc.Row([
-            dbc.Col([dbc.Input(placeholder="X", type="number", required=True, size="sm")], width=4),
-            dbc.Col([dbc.Input(placeholder="Y", type="number", required=True, size="sm")], width=4),
-            dbc.Col([dbc.Input(placeholder="Z", type="number", required=True, size="sm")], width=4),
+            dbc.Col([dbc.Input(placeholder="X", type="number",
+                    required=True, size="sm")], width=4),
+            dbc.Col([dbc.Input(placeholder="Y", type="number",
+                    required=True, size="sm")], width=4),
+            dbc.Col([dbc.Input(placeholder="Z", type="number",
+                    required=True, size="sm")], width=4),
         ]),
 
         html.Label("Substrate"),
         dbc.Row([
-            dbc.Col([dbc.Input(placeholder="X", type="number", required=True, size="sm")], width=4),
-            dbc.Col([dbc.Input(placeholder="Y", type="number", required=True, size="sm")], width=4),
-            dbc.Col([dbc.Input(placeholder="Z", type="number", required=True, size="sm")], width=4),
+            dbc.Col([dbc.Input(placeholder="X", type="number",
+                    required=True, size="sm")], width=4),
+            dbc.Col([dbc.Input(placeholder="Y", type="number",
+                    required=True, size="sm")], width=4),
+            dbc.Col([dbc.Input(placeholder="Z", type="number",
+                    required=True, size="sm")], width=4),
         ]),
 
         html.Label("Solder balls"),
         dbc.Row([
-            dbc.Col([dbc.Input(placeholder="X", type="number", required=True, size="sm")], width=4),
-            dbc.Col([dbc.Input(placeholder="Y", type="number", required=True, size="sm")], width=4),
-            dbc.Col([dbc.Input(placeholder="Z", type="number", required=True, size="sm")], width=4),
+            dbc.Col([dbc.Input(placeholder="X", type="number",
+                    required=True, size="sm")], width=4),
+            dbc.Col([dbc.Input(placeholder="Y", type="number",
+                    required=True, size="sm")], width=4),
+            dbc.Col([dbc.Input(placeholder="Z", type="number",
+                    required=True, size="sm")], width=4),
         ]),
 
-        dbc.CardImg(src="https://drive.google.com/file/d/1-auPFqRIR9s0WCYqPaAplXvzNfQpqBCW/view?usp=sharing")
+        dbc.Card([
+            html.Label("Package layout"),
+            dbc.CardImg(src="http://localhost:8000/package_simplified.png")
+        ], style={"margin-top": "7px"})
+
 
     ], title="Geometry and Model"),
     dbc.AccordionItem([
         html.Label("Top BC"),
         dcc.Dropdown(id="top-bc", options=[
-            {"label": "Insulated", "value": "insulated"}, 
+            {"label": "Insulated", "value": "insulated"},
             {"label": "Const. T", "value": "const_T"},
             {"label": "Const. HTC", "value": "const_h"},
             {"label": "Const. heat flux", "value": "const_q"}]),
@@ -71,7 +95,7 @@ left_accordion = dbc.Accordion([
 
         html.Label("Bottom BC"),
         dcc.Dropdown(id="bottom-bc", options=[
-            {"label": "Insulated", "value": "insulated"}, 
+            {"label": "Insulated", "value": "insulated"},
             {"label": "Const. T", "value": "const_T"},
             {"label": "Const. HTC", "value": "const_h"},
             {"label": "Const. heat flux", "value": "const_q"}]),
@@ -82,17 +106,20 @@ left_accordion = dbc.Accordion([
     ], title="Boundary Conditions"),
     dbc.AccordionItem([
         html.Label("Ambient temperature"),
-        dbc.Input(placeholder="Ambient temperature", type="number", required=True, value=25),
+        dbc.Input(placeholder="Ambient temperature",
+                  type="number", required=True, value=25),
 
         dbc.Row([
-        dbc.Col([
-            html.Label("Start time"),
-            dbc.Input(placeholder="Start time", type="number", value=0.0)]),
+            dbc.Col([
+                html.Label("Start time"),
+                dbc.Input(placeholder="Start time", type="number", value=0.0)]),
 
-        dbc.Col([html.Label("End time"),
-        dbc.Input(placeholder="End time", type="number")])]),
+            dbc.Col([html.Label("End time"),
+                     dbc.Input(placeholder="End time", type="number")])]),
     ], title="Problem Setup"),
 ])
+
+# bottom_console = dbc.
 
 navbar = dbc.NavbarSimple(
     children=[
@@ -134,19 +161,61 @@ app.layout = dbc.Container(
     [
         navbar,
         html.Hr(),
-        dbc.Col([
-            dbc.Card([
-                dbc.Col([
-                    left_accordion
-                ], width=12)], body=True)
-        ], width=3),
-        # dbc.Col([
-        #     dbc.Card([
-        #         db
-        #     ])
-        # ])
+        dbc.Row([
+            dbc.Col([
+                dbc.Card([
+                    dbc.Button("Settings", id="open", n_clicks=0),
+                    dbc.Modal(
+                        [
+                            dbc.ModalHeader(dbc.ModalTitle(
+                                "Model settings")),
+                            # dbc.ModalBody("This is the content of the modal"),
+                            left_accordion,
+                            dbc.ModalFooter(
+                                dbc.Button(
+                                    "Submit", id="close", className="ms-auto", n_clicks=0
+                                )
+                            ),
+                        ],
+                        id="modal",
+                        is_open=False,
+                    ),
+                ])
+            ], width=3),
+            dbc.Col([
+                dbc.Card([
+                    dbc.Button("Settings", id="open2", n_clicks=0),
+                    dbc.Modal(
+                        [
+                            dbc.ModalHeader(dbc.ModalTitle(
+                                "Model settings")),
+                            # dbc.ModalBody("This is the content of the modal"),
+                            left_accordion,
+                            dbc.ModalFooter(
+                                dbc.Button(
+                                    "Submit", id="close", className="ms-auto", n_clicks=0
+                                )
+                            ),
+                        ],
+                        id="modal2",
+                        is_open=False,
+                    ),
+                ])], width=10
+            )]
+        ),
     ]
 )
+
+
+# @app.callback(
+#     Output("modal", "is_open"),
+#     [Input("open", "n_clicks"), Input("close", "n_clicks")],
+#     [State("modal", "is_open")],
+# )
+# def toggle_modal(n1, n2, is_open):
+#     if n1 or n2:
+#         return not is_open
+#     return is_open
 
 
 if __name__ == "__main__":

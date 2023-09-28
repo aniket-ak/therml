1c2215e22c7b61c37cbf0aaa0a796717ec208c9d
Aniket <ani.kulkar@gmail.com>
2023-09-28 16:57:16 +0530

    updates to ui

--- UI/test_app.py
+++ UI/test_app.py
@@ -1,35 +1,24 @@
-from dash import Dash, dash_table, Input, Output, callback
+from dash import Dash, html, dcc, callback, Output, Input
+import plotly.express as px
 import pandas as pd
 
-
-df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapminder2007.csv')
-
-df[' index'] = range(1, len(df) + 1)
+df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapminder_unfiltered.csv')
 
 app = Dash(__name__)
 
-PAGE_SIZE = 5
-
-app.layout = dash_table.DataTable(
-    id='datatable-paging',
-    columns=[
-        {"name": i, "id": i} for i in sorted(df.columns)
-    ],
-    page_current=0,
-    page_size=PAGE_SIZE,
-    page_action='custom'
-)
-
+app.layout = html.Div([
+    html.H1(children='Title of Dash App', style={'textAlign':'center'}),
+    dcc.Dropdown(df.country.unique(), 'Canada', id='dropdown-selection'),
+    dcc.Graph(id='graph-content')
+])
 
 @callback(
-    Output('datatable-paging', 'data'),
-    Input('datatable-paging', "page_current"),
-    Input('datatable-paging', "page_size"))
-def update_table(page_current,page_size):
-    return df.iloc[
-        page_current*page_size:(page_current+ 1)*page_size
-    ].to_dict('records')
-
+    Output('graph-content', 'figure'),
+    Input('dropdown-selection', 'value')
+)
+def update_graph(value):
+    dff = df[df.country==value]
+    return px.line(dff, x='year', y='pop')
 
 if __name__ == '__main__':
     app.run(debug=True)

da87bdab52fe05a6b2f535d2768d6e0db5077b54
root <root@e2e-73-43.ssdcloudindia.net>
2023-10-28 15:24:30 +0530

    refactoring considering building

---
+++ 3d/therml_environment/src/copy_.jl
@@ -0,0 +1,65 @@
+module therml_environment
+
+using DifferentialEquations, LinearAlgebra, SparseArrays
+using PlotlyJS
+using Symbolics
+using JSON
+using Dates
+using LoopVectorization
+using CSV, DataFrames
+using Interpolations
+using Logging: global_logger
+using TerminalLoggers: TerminalLogger
+using JLD2
+using HDF5
+using Logging
+using LoggingExtras
+
+function julia_main()::Cint
+    try
+        real_main()
+    catch
+        Base.invokelatest(Base.display_error, Base.catch_stack())
+        return 1
+    end
+    return 0
+end
+
+function real_main()
+    working_dir = ARGS[1]
+    scenario_name = ARGS[2]
+    run_name = ARGS[3]
+
+    println("working dir : ", working_dir)
+    println("scenario name : ", scenario_name)
+    println("run name : ", run_name)
+
+    if run_name == ""
+        run_name = now()
+    end
+
+    run_wd = joinpath(working_dir, run_name)
+    log_wd = joinpath(run_wd,"Logs")
+    sol_wd = joinpath(run_wd,"Solution")
+    temp_wd = joinpath(run_wd,"Temp")
+
+    progress_file_name = joinpath(temp_wd, scenario_name*"__progress.txt")
+    progress_file = open(progress_file_name, "w")
+    global_logger(TerminalLogger(progress_file))
+
+    include("/root/therml/therml/3d/therml_environment/src/unctions_fvm_3d.jl")
+
+    f = open(joinpath(working_dir,"settings.json"), "r")
+    settings = JSON.parse(f)
+    println(Dates.format(now(), "HH:MM:SS"))
+    # sol = solve_(working_dir, scenario_name);
+    sol = Base.invokelatest(solve_, working_dir, scenario_name, settings);
+    # do_plotting(sol, sol_wd, false);
+    Base.invokelatest(do_plotting, sol, sol_wd, settings, scenario_name, false)
+    # save_fields(sol,sol_wd);
+    Base.invokelatest(save_fields, sol, sol_wd, scenario_name)
+    println(Dates.format(now(), "HH:MM:SS"))
+    flush(stdout)
+end
+
+end
